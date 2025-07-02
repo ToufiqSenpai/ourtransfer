2c1707085cf0dfcf0ca36535f038b6b88200c300
Muhammad Taufiqurrahman <mhmtaufiqurrahman@gmail.com>
2025-07-02 22:17:41 +0700

    chore: add AutomapperModule to app.module.ts

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from "@nestjs/common"
 import { CqrsModule } from "@nestjs/cqrs"
 import { InfrastructureModule } from "./infrastructure/infrastructure.module"
+import { AutomapperModule } from "@automapper/nestjs"
+import { classes } from "@automapper/classes"
 
 @Module({
   imports: [
     CqrsModule.forRoot(),
+    AutomapperModule.forRoot({
+      strategyInitializer: classes()
+    }),
     InfrastructureModule
   ]
 })
