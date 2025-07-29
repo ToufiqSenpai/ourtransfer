d40888e03d25fff41bee99ba2f05ac815c547f12
Muhammad Taufiqurrahman <mhmtaufiqurrahman@gmail.com>
2025-07-29 11:11:28 +0700

    chore: refactor authentication handling and introduce identity management

--- apps/backend/src/modules/auth/infrastructure/repositories/password-identity.repository.impl.ts
+++ apps/backend/src/modules/auth/infrastructure/repositories/password-identity.repository.impl.ts
@@ -1,11 +1,28 @@
 import { PasswordIdentityRepository } from '../../domain/repositories/password-identity.repository';
 import { PasswordIdentity } from '../../domain/entities/password-identity.entity';
-import { DataSource } from 'typeorm';
+import { Repository } from "typeorm"
+import { InjectRepository } from "@nestjs/typeorm"
 
 export class PasswordIdentityRepositoryImpl implements PasswordIdentityRepository {
-  public constructor(private readonly dataSource: DataSource) {}
+  public constructor(@InjectRepository(PasswordIdentity) private readonly repository: Repository<PasswordIdentity>) {}
 
   public async create(entity: PasswordIdentity): Promise<PasswordIdentity> {
-    return await this.dataSource.getRepository(PasswordIdentity).save(entity)
+    return await this.repository.save(entity)
+  }
+
+  public async findById(id: string): Promise<PasswordIdentity | null> {
+    return await this.repository.findOneBy({ id })
+  }
+
+  public findAll(): Promise<PasswordIdentity[]> {
+    return this.repository.find()
+  }
+
+  public update(id: string, entity: PasswordIdentity): Promise<PasswordIdentity> {
+    throw new Error("Method not implemented.")
+  }
+
+  public delete(id: string): Promise<void> {
+    throw new Error("Method not implemented.")
   }
 }
